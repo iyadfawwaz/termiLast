88a8fe91e98f0fb537d4dd352fa9fe63eae58e4b
iyadseizer <se.black3d@gmail.com>
2023-08-08 16:35:43 +0300

    Update Token.java

--- app/src/main/java/sy/iyad/server/LoginActivity.java
+++ app/src/main/java/sy/iyad/server/LoginActivity.java
@@ -5,10 +5,16 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -17,10 +23,16 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -42,72 +54,83 @@ import static sy.iyad.server.Utils.ServerInfo.*;
 public class LoginActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 201;
-    public static final String ACTION_LOGIN = "AcXFU2M0842021";
-   // public static final String ACTION_RUN = "ArXFU2M0842021";
-    public static final String SHARED_KEY="Sk0842021ChineScood";
+    public static final int CAMERA_REQ = 202;
 
-    EditText admin,ip,pass;
+    public static final String ACTION_LOGIN = "AcXFU2M0842021";
+    // public static final String ACTION_RUN = "ArXFU2M0842021";
+    public static final String SHARED_KEY = "Sk0842021ChineScood";
+
+    EditText admin, ip, pass;
     Button login;
     TextView textView;
     CheckBox checkBox;
     Toolbar toolbar;
+    SurfaceView surfaceView;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    ToneGenerator toneGenerator;
 
     FloatingActionButton actionButton;
 
     @SuppressLint("RestrictedApi")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView( R.layout.activity_login);
+        setContentView(R.layout.activity_login);
 
         askPermissions();
         if (!isRegistered) {
             registerId();
         }
-            login =  findViewById(R.id.connect);
-            ip =  findViewById(R.id.ip);
-            admin =  findViewById(R.id.admin);
-            pass =  findViewById(R.id.password);
-            textView =  findViewById(R.id.textView);
-            actionButton = findViewById(R.id.floatingActionButton);
-            checkBox = findViewById(R.id.checkBox);
-            toolbar = findViewById(R.id.toolbar);
-            toolbar.setCollapseIcon(R.drawable.avatarx);
-            toolbar.setCollapsible(true);
-            toolbar.setTitle(getString(R.string.app_name));
-            setSupportActionBar(toolbar);
+        login = findViewById(R.id.connect);
+        ip = findViewById(R.id.ip);
+        admin = findViewById(R.id.admin);
+        pass = findViewById(R.id.password);
+        textView = findViewById(R.id.textView);
+        actionButton = findViewById(R.id.floatingActionButton);
+        checkBox = findViewById(R.id.checkBox);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setCollapseIcon(R.drawable.avatarx);
 
-            laodMainInfo();
+        surfaceView = findViewById(R.id.surfaceView);
 
-            login.setOnClickListener(v ->  {
+        toolbar.setCollapsible(true);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
-                    String ips = ip.getText().toString();
-                    String admins = admin.getText().toString();
-                    String passs = pass.getText().toString();
+        laodMainInfo();
+      requestPermissions(new String[]{Manifest.permission.CAMERA},CAMERA_REQ);
+        initialiseDetectorsAndSources();
 
-                    doLogin(ips, admins, passs);
+        login.setOnClickListener(v -> {
 
-                });
+            String ips = ip.getText().toString();
+            String admins = admin.getText().toString();
+            String passs = pass.getText().toString();
 
-            login.setOnLongClickListener(v ->  {
+            doLogin(ips, admins, passs);
 
+        });
+
+        login.setOnLongClickListener(v -> {
+
+            registerId();
+            return true;
+        });
+
+        actionButton.setOnClickListener(v -> {
+            ip.setText(loadInfo("ip"));
+            admin.setText(loadInfo("admin"));
+            pass.setText(loadInfo("password"));
+
+        });
+        actionButton.setOnLongClickListener(v -> {
+                    showHappens("your vip : " + VIP);
+                    textView.setBackgroundColor(VIP);
+                    isRegistered = false;
                     registerId();
-                                             return true;
-                                     });
-
-            actionButton.setOnClickListener(v ->  {
-                    ip.setText(loadInfo("ip"));
-                    admin.setText(loadInfo("admin"));
-                    pass.setText(loadInfo("password"));
-
-            });
-            actionButton.setOnLongClickListener(v -> {
-                        showHappens("your vip : " + VIP );
-                        textView.setBackgroundColor(VIP);
-                isRegistered = false;
-                registerId();
-                        return true;
-                    }
-            );
+                    return true;
+                }
+        );
 
     }
 
@@ -116,11 +139,90 @@ public class LoginActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Q) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE},207);
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 207);
         }
     }
+
+
+    private void initialiseDetectorsAndSources() {
+
+        toneGenerator = new ToneGenerator(AudioManager.AUDIOFOCUS_GAIN,100);
+        //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(LoginActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(holder);
+                    } else {
+                        ActivityCompat.requestPermissions(LoginActivity.this, new
+                                String[]{Manifest.permission.CAMERA}, CAMERA_REQ);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+
+
+                    textView.post((Runnable) () -> {
+
+                        if (barcodes.valueAt(0).url != null) {
+                            textView.removeCallbacks(null);
+                            String barcodeData = barcodes.valueAt(0).url.url;
+                            textView.setText(barcodeData);
+
+                            toneGenerator.startTone(ToneGenerator.TONE_SUP_PIP, 150);
+                        } else {
+
+                            String barcodeData = barcodes.valueAt(0).displayValue;
+                            textView.setText(barcodeData);
+                            toneGenerator.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
 
     private void laodMainInfo() {
         ip.setText(IP);
